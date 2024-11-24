994bff926135486359db4ce18be8d3f6dc737e53
Yokinman <yokinmun@gmail.com>
2024-11-23 22:32:41 -0500

    + Added `PartialEq` and `PartialOrd` bounds to `linear::Linear`.

--- src/linear.rs
+++ src/linear.rs
@@ -108,7 +108,7 @@ mod _scalar_impls {
 /// 
 /// This basically just represents floating-point numbers and vector types.
 /// For vectors, operations are applied per component in parallel.
-pub trait Linear: Clone + Debug + 'static {
+pub trait Linear: PartialEq + PartialOrd + Clone + Debug + 'static {
 	fn add(self, other: Self) -> Self;
 	
 	fn sub(self, other: Self) -> Self;
